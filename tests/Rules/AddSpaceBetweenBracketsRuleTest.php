844d045f6e42382910b432be39b30bf19eadd189
Tobias Köngeter <tobias.koengeter@bitandblack.com>
2026-07-17 09:15:31 +0200

    Improve rule

--- tests/Rules/AddSpaceBetweenBracketsRuleTest.php
+++ tests/Rules/AddSpaceBetweenBracketsRuleTest.php
@@ -31,5 +31,17 @@ final class AddSpaceBetweenBracketsRuleTest extends AbstractRuleTestClass
             'a' . CharactersEnum::HAIR_SPACE_UTF8->value . ') Es geht los (' . CharactersEnum::HAIR_SPACE_UTF8->value . 'warum auch immer' . CharactersEnum::HAIR_SPACE_UTF8->value . ')! Was(' . CharactersEnum::HAIR_SPACE_UTF8->value . 'es' . CharactersEnum::HAIR_SPACE_UTF8->value . ')wir es?',
             'a) Es geht los (warum au...',
         ];
+
+        yield [
+            '(Einfach nein.)',
+            '(' . CharactersEnum::HAIR_SPACE_UTF8->value . 'Einfach nein.)',
+            '(Einfach nein.)',
+        ];
+
+        yield [
+            '(Einfach nein,) vermutlich',
+            '(' . CharactersEnum::HAIR_SPACE_UTF8->value . 'Einfach nein,) vermutlich',
+            '(Einfach nein,) vermutli...',
+        ];
     }
 }
